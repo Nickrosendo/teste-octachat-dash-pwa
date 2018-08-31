bf145efdbed36cd6af07ee1fddf6cf56903a48cb
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2018-08-31 16:47:36 -0300

    dist commit

---
+++ octa-config.js
@@ -0,0 +1,6 @@
+window.octadesk = window.octadesk || {}
+window.octadesk.ga = 'UA-64766267-5'
+window.octadesk.recaptchaKey = '6Ld3pREUAAAAADEN8rkwuO8zfjX91OvkLpIze9V-'
+window.octadesk.apis = {
+  chatUrl: 'https://chat.qaoctadesk.services/api'
+}
---
+++ precache-manifest.7cdd4832c9ca7a33ae6eea102fbfd1ad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "597e4b1ef55c27e3a5f975ddd5cb05bb",
+    "url": "static/img/icomoon.597e4b1e.svg"
+  },
+  {
+    "revision": "dd9c205b31cbaf9c18e8",
+    "url": "static/css/chunk-vendors.7f4df90b.css"
+  },
+  {
+    "revision": "63f16b21e1ff87088811",
+    "url": "static/css/index.d359a8d5.css"
+  },
+  {
+    "revision": "63f16b21e1ff87088811",
+    "url": "static/js/index.8fcda2b0.js"
+  },
+  {
+    "revision": "103714fd8cb737d9f9fd2096add27751",
+    "url": "static/fonts/icomoon.103714fd.woff2"
+  },
+  {
+    "revision": "328250bf96aacb4861f0399840d8c748",
+    "url": "static/fonts/icomoon.328250bf.ttf"
+  },
+  {
+    "revision": "dd9c205b31cbaf9c18e8",
+    "url": "static/js/chunk-vendors.3b0ab0ef.js"
+  },
+  {
+    "revision": "8ca2c59c70a3c972edda164f151e2fca",
+    "url": "static/fonts/icomoon.8ca2c59c.woff"
+  },
+  {
+    "revision": "1912cd194e9aa4f23f1a03eb52ea9805",
+    "url": "static/fonts/icomoon.1912cd19.eot"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "4e2b7f520631c6935cfa0c76cd7959f0",
+    "url": "octa-config.js"
+  },
+  {
+    "revision": "4b30f95816392d6a1576e78eee7754de",
+    "url": "index.html"
+  },
+  {
+    "revision": "8338fb4698c46c8046bada48771b7717",
+    "url": "index.dev.html"
+  }
+];
